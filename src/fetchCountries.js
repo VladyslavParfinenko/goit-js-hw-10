51be256fad93a65b7871f9a1091340a3f5110785
Vladyslav Parfinenko <ronwaylla@gmail.com>
2023-05-19 23:14:17 +0300

    fix hw

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,25 +1,15 @@
 function fetchCountries(exempleCountry) {
-    const URL = 'https://restcountries.com/v3.1/';
-    const ENDPOINT = 'name/';
-    return fetch(`${URL}${ENDPOINT}${exempleCountry}?fields=name,capital,population,languages,flags`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Request failed');
-        }
-        return response.json().then(data => {
-            console.log(data);
-          const { name, capital, population, languages, flags } = data[0];
-          return {
-            officialName: name.official,
-            capital,
-            population,
-            languages,
-            flagSVG: flags.svg
-          };
-        });
-      })
-      .catch(error => console.log(error));
-  }
+  const URL = 'https://restcountries.com/v3.1/';
+  const ENDPOINT = 'name/';
+  return fetch(`${URL}${ENDPOINT}${exempleCountry}?fields=name,capital,population,languages,flags`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Request failed');
+      }
+      return response.json();
+    });
+}
+
   
   export { fetchCountries };
   
--- src/index.js
+++ src/index.js
@@ -1,48 +1,110 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
-
 
 const input = document.querySelector('#search-box');
 const countryInfo = document.querySelector('.country-info');
 
-input.addEventListener('input',onSearch);
+input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
+function simpleMurkup({ officialName, flagSVG }) {
+  countryInfo.insertAdjacentHTML(
+    'beforeend',
+    `
+    <h1><img src=${flagSVG} height='50'>${officialName}</h1>
+    
+  `
+  );
+}
 
+function createMarkup({
+  officialName,
+  capital,
+  population,
+  languages,
+  flagSVG,
+}) {
+  const languagesHTML = Object.values(languages).join(', ');
 
-
-
-
-      
-function createMarkup({ officialName, capital, population, languages, flagSVG }) {
-    const languagesHTML = Object.values(languages).join(', ');
-  
-    countryInfo.insertAdjacentHTML('beforeend', `
+  countryInfo.insertAdjacentHTML(
+    'beforeend',
+    `
       <img src=${flagSVG} height='50'>
       <h1>${officialName}</h1>
       <h2>Capital: ${capital}</h2>
       <h2>Population: ${population}</h2>
       <h2>Languages: ${languagesHTML}</h2>
-    `);
-  }
-  
+    `
+  );
+}
 
-  
 function onSearch(event) {
-    const exemplCountry = event.target.value.toLowerCase();
-    fetchCountries(exemplCountry)
-      .then(data => {
-        createMarkup(data); // Вставка полученных данных в разметку
-      })
-      .catch(error => console.log(error));
-  }
+  const exemplCountry = event.target.value.toLowerCase();
 
- 
+  fetchCountries(exemplCountry)
+    .then(data => {
+      if (data.length > 10) {
+        
+        Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
+        
+      } else if (data.length > 2 && data.length < 10) {
+        clearResults();
+        const namesAndFlags = data.map(item => {
+          const { name, flags } = item;
+          return {
+            officialName: name.official,
+            flagSVG: flags.svg,
+          };
+        });
+        
+        countryInfo.innerHTML = '';
+        namesAndFlags.forEach(obj => simpleMurkup(obj, countryInfo));
+      } else if (data.length === 1) {
+        clearResults();
+        const { name, capital, population, languages, flags } = data[0];
+        const obj = {
+          officialName: name.official,
+          capital,
+          population,
+          languages,
+          flagSVG: flags.svg,
+        };
+        countryInfo.innerHTML = '';
+        createMarkup(obj, countryInfo);
+      }
+    })
+    .catch(error => console.log(error));
+}
 
+function clearResults() {
+  countryInfo.innerHTML = '';
+}
 
+//   fetchCountries(exemplCountry)
+//     .then(data => {
 
+//         createMarkup(data); // Вставка полученных данных в разметку
+//     })
+//     .catch(error => console.log(error));
+// }
 
+//  function murkupSimple(){
+//   if (data.length > 2){
 
+//   }
+//  };
 
+// console.log(data.length);
 
+// const { name, capital, population, languages, flags } = data[0];
+// const countryData = {
+//   officialName: name.official,
+//   capital,
+//   population,
+//   languages,
+//   flagSVG: flags.svg
+// };
+// console.log(data);
